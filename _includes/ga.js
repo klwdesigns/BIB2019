b6182745dfa5b9264f63506f23f1025c70952956
KLW Designs <40128412+klwdesigns@users.noreply.github.com>
2019-11-01 23:03:54 -0500

    Add files via upload

---
+++ _includes/ga.js
@@ -0,0 +1,7 @@
+var doNotTrack = false;
+if (!doNotTrack) {
+	window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+	ga('create', 'UA-56280078-2', 'auto');
+
+	ga('send', 'pageview');
+}
